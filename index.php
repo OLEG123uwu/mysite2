cf8b9e663b219f9435b6c371ac70c1d93a4df489
OLEG123uwu <137278187+OLEG123uwu@users.noreply.github.com>
2023-06-21 15:17:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
-echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Привет, мир!</title>
+  <style>
+    body {
+      background-color: #f2f2f2;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 20px;
+    }
+    
+    h1 {
+      color: #333;
+      text-align: center;
+    }
+    
+    p {
+      color: #666;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h1>Привет, мир!</h1>
+  <p>Это моя первая HTML-страница.</p>
+</body>
+</html>
